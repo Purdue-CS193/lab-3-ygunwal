76d683f42b4b45dae654eddddc653d0f59e7e015
ygunwal <42846148+ygunwal@users.noreply.github.com>
2018-10-04 22:37:53 -0400

    Update BuggyUtilClient.java

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -5,6 +5,7 @@ public class BuggyUtilClient {
         System.out.println("Example 1: Make My Username");
         System.out.println("In: first = " + "\"Sean\"" +
                 ", last = " + "\"Flannery\"");
+        System.out.println("Yaassssss");
         System.out.println("Out: " +
         StringUtils.makePurdueUsername("Sean", "Flannery"));
     }
